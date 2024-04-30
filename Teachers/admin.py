d4e498abeebe2704bf09bb812c4b542ee6b6c0db
GDD-impact <gddimpactsoftwares@gmail.com>
2024-04-30 01:48:55 +0100

    changes

--- Teachers/admin.py
+++ Teachers/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Teacher)
+
 @admin.register(Teacher)
 class TeacherAdmin(admin.ModelAdmin):
     list_display=('firstName','surname','teachers_id','role')
