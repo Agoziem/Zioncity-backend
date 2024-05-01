ff25638398b21374e2f2a3fa2692e1a532f6641a
GDD-impact <gddimpactsoftwares@gmail.com>
2024-05-01 12:30:42 +0100

    changes

--- Results/admin.py
+++ Results/admin.py
@@ -3,28 +3,28 @@ from .models import ResultSummary,SubjectResult,AnnualResultSummary,AnnualSubjec
 
 @admin.register(ResultSummary)
 class ResultSummaryAdmin(admin.ModelAdmin):
-    list_display=('Student_name','TotalScore','Totalnumber','Average','Position','Term','AcademicSession','published')
+    list_display=('Student_name','Term','AcademicSession','published')
     ordering=('Student_name','Term')
     search_fields=('Student_name__firstname','Term__term','AcademicSession__session')
     list_filter=('Term','AcademicSession','published')
 
 @admin.register(SubjectResult)
 class SubjectResultAdmin(admin.ModelAdmin):
-    list_display=('student','Subject','FirstTest','FirstAss','MidTermTest','Project','SecondAss','SecondTest','CA','Exam','Total','Grade','SubjectPosition','Term','AcademicSession','student_class','student_school','is_offering','published')
+    list_display=('student','Subject','Term','AcademicSession','student_class','is_offering','published')
     ordering=('student','Subject','Term')
-    search_fields=('student__firstname','Subject__subject_name','Term__term','AcademicSession__session','student_class__Class','student_school__Schoolname')
+    search_fields=('student__firstname','Subject__subject_name','Term__term','AcademicSession__session','student_class__Class')
     list_filter=('Term','AcademicSession','student_class','student_school','is_offering','published')
 
 @admin.register(AnnualResultSummary)
 class AnnualResultSummaryAdmin(admin.ModelAdmin):
-    list_display=('Student_name','TotalScore','Totalnumber','Average','Position','PrincipalVerdict','AcademicSession','published')
+    list_display=('Student_name','PrincipalVerdict','AcademicSession','published')
     ordering=('Student_name','AcademicSession')
     search_fields=('Student_name__firstname','AcademicSession__session')
     list_filter=('AcademicSession','published')
 
 @admin.register(AnnualSubjectResult)
 class AnnualSubjectResultAdmin(admin.ModelAdmin):
-    list_display=('student','Subject','FirstTermTotal','SecondTermTotal','ThirdTermTotal','Total','Average','Grade','SubjectPosition','Remark','AcademicSession','is_offering','published')
+    list_display=('student','Subject','AcademicSession','is_offering','published')
     ordering=('student','Subject','AcademicSession')
     search_fields=('student__firstname','Subject__subject_name','AcademicSession__session')
     list_filter=('AcademicSession','is_offering','published')
